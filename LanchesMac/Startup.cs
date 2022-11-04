a34ee5d2a86dd8fd899305767581d70f3f1031a1
Alessandro Lobo <alessandro.lobo@hotmail.com>
2022-11-04 16:11:47 -0300

    Config String conection

--- LanchesMac/Startup.cs
+++ LanchesMac/Startup.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using LanchesMac.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace LanchesMac
 {
@@ -14,6 +15,9 @@ namespace LanchesMac
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+             options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddControllersWithViews();
         }
 
