34ac7bdfb2b652210f32d3f21d9eefca8ab03be4
Alessandro Lobo <alessandro.lobo@hotmail.com>
2022-11-07 18:11:34 -0300

    Create CarriinhoCompraResumo

--- LanchesMac/Models/CarrinhoCompra.cs
+++ LanchesMac/Models/CarrinhoCompra.cs
@@ -1,4 +1,5 @@
 ﻿using LanchesMac.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace LanchesMac.Models
 {
@@ -59,20 +60,19 @@ namespace LanchesMac.Models
             _context.SaveChanges();
         }
 
-        public int RemoverDoCarrinho(Lanche lanche)
+        public void RemoverDoCarrinho(Lanche lanche)
         {
-            var carrinhoCompraItem =
-                _context.CarrinhoCompraItens.SingleOrDefault(
+            var carrinhoCompraItem = _context.CarrinhoCompraItens.SingleOrDefault(
                     s => s.Lanche.LancheId == lanche.LancheId &&
                     s.CarrinhoCompraId == CarrinhoCompraId);
-            var quantidadeLocal = 0;
+            //var quantidadeLocal = 0;
 
             if (CarrinhoCompraId != null)
             {
                 if (carrinhoCompraItem.Quantidade > 1)
                 {
                     carrinhoCompraItem.Quantidade--;
-                    quantidadeLocal = carrinhoCompraItem.Quantidade;
+                    // quantidadeLocal = carrinhoCompraItem.Quantidade;
                 }
                 else
                 {
@@ -80,7 +80,33 @@ namespace LanchesMac.Models
                 }
             }
             _context.SaveChanges();
-            return quantidadeLocal;
+            // return quantidadeLocal;
+        }
+
+        public List<CarrinhoCompraItem> GetCarrinhoCompraItens()
+        {
+            return CarrinhoCompraItems ??
+                (CarrinhoCompraItems =
+                _context.CarrinhoCompraItens
+                .Where(c => c.CarrinhoCompraId == CarrinhoCompraId)
+                .Include(s => s.Lanche)
+                .ToList());
+        }
+
+        public void LimparCarrinho()
+        {
+            var carrinhoItens = _context.CarrinhoCompraItens
+                .Where(carrinho => carrinho.CarrinhoCompraId == CarrinhoCompraId);
+            _context.CarrinhoCompraItens.RemoveRange(carrinhoItens);
+            _context.SaveChanges();
+        }
+
+        public decimal GetCarrinhoCompraTotal()
+        {
+            var total = _context.CarrinhoCompraItens
+                .Where(c => c.CarrinhoCompraId == CarrinhoCompraId)
+                .Select(c => c.Lanche.Preco * c.Quantidade).Sum();
+            return total;
         }
     }
 }
--- LanchesMac/Startup.cs
+++ LanchesMac/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using LanchesMac.Context;
+using LanchesMac.Models;
 using LanchesMac.Repositories;
 using LanchesMac.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
@@ -25,6 +26,8 @@ namespace LanchesMac
             services.AddTransient<ICategoriaRepository, CategoriaRepository>();
             // Relacionado ao cash
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddScoped(sp => CarrinhoCompra.GetCarrinho(sp));
 
             services.AddControllersWithViews();
 
