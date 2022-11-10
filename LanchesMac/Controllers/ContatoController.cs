eb467ac97b208ab11aa08cf7fa292a50d671dda8
Alessandro Lobo <alessandro.lobo@hotmail.com>
2022-11-10 07:56:56 -0300

    Create Area Admin inicio

--- LanchesMac/Controllers/ContatoController.cs
+++ LanchesMac/Controllers/ContatoController.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Controllers
 {
     public class ContatoController : Controller
     {
+        [Authorize]
         public IActionResult Index()
         {
+            // Essa implementação da acesso ou não a pagina de contatos, verificando se voc esta ou não logado
+            //if (User.Identity.IsAuthenticated)
+            //{
             return View();
+            // }
+
         }
     }
 }
